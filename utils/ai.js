6309f2f0735f64396464a31ebc3dc5665aa63c2d
Deyan <dengdeyan@gmail.com>
2025-01-27 15:26:09 -0500

    Add utils ai.js

--- utils/ai.js
+++ utils/ai.js
@@ -1,6 +1,25 @@
-// import OpenAI from "openai";
-
-// const openai = new OpenAI({
-//         baseURL: 'https://api.deepseek.com',
-//         apiKey: '<DeepSeek API Key>'
-// });
+export function getAIRequest(systemPrompt, userInput){
+  const obj = {
+    model: "deepseek-chat",
+    messages: [
+      {
+        role: "system",
+        content: systemPrompt
+      },
+      {
+        role: "user",
+        content: userInput
+      }
+    ],
+    stream: false
+  };
+  const request = new Request("https://api.deepseek.com", {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: {
+      "Authorization":
+        "Bearer <DeepSeek API Key>",
+    },
+  });  
+  return request;
+}
